bbf0e1f99be9b90aeddbc125e48a74411d673afe
TMfrei <shuntingyard@gmail.com>
2023-09-24 22:18:14 +0200

    println! adapters when starting desktop

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,20 @@ fn main() {
         .with(EnvFilter::from_default_env()) // Read trace levels from RUST_LOG env var.
         .init();
 
+    // Have aquick glance at adapters available.
+    wgpu::Instance::new(wgpu::InstanceDescriptor {
+        backends: wgpu::Backends::all(),
+        dx12_shader_compiler: Default::default(),
+    })
+    .enumerate_adapters(wgpu::Backends::all())
+    .for_each(|adapter| {
+        let attr = adapter.get_info();
+        println!(
+            "{:>32}, {:?}, {:?}",
+            attr.name, attr.backend, attr.device_type
+        );
+    });
+
     // Enable async here by putting the tread into a wait state
     // while all can still be handled differently for wasm.
     pollster::block_on(run());
